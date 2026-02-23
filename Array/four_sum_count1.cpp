ee6d767bc3132af565a51944ca50e496e9486a03
Divya Lakshmi <divyasumi123@gmail.com>
2026-02-23 15:16:34 +0530

    four_sum_count1

---
+++ Array/four_sum_count1.cpp
@@ -0,0 +1,63 @@
+/*
+
+Leetcode Link               : https://leetcode.com/problems/4sum-ii/description/
+
+1291. Sequential Digits
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Hint
+Given four integer arrays nums1, nums2, nums3, and nums4 all of length n, return the number of tuples (i, j, k, l) such that:
+
+0 <= i, j, k, l < n
+nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ 
+
+Example 1:
+
+Input: nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+Output: 2
+Explanation:
+The two tuples are:
+1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) + (-1) + 2 = 0
+2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) + (-1) + 0 = 0
+Example 2:
+
+Input: nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+Output: 1
+ 
+
+Constraints:
+n == nums1.length
+n == nums2.length
+n == nums3.length
+n == nums4.length
+1 <= n <= 200
+-228 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 228
+*/
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        map<int, int> mp;
+
+        for(int n1: nums1){
+            for(int n2: nums2){
+                mp[n1 + n2]++;
+            }
+        }
+        
+        int count = 0;
+
+        for(int n3: nums3){
+            for(int n4: nums4){
+                int target = -(n3 + n4);
+                count += mp[target];
+            }
+        }
+
+        return count;
+    }
+};
